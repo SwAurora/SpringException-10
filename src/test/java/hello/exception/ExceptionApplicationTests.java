f4bf8125e71641a67a1173b3ec93428f0e6b7aaa
SwAurora <rlatjddnr74@gmail.com>
2022-12-04 02:13:46 +0900

    2022-12-04 master(ver1)

---
+++ src/test/java/hello/exception/ExceptionApplicationTests.java
@@ -0,0 +1,15 @@
+package hello.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
